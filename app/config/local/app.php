6641ac0662a836ff0d16c469cb2e8a084fa02453
Wade Urry <wader@iwader.co.uk>
2014-03-24 20:37:21 +0000

    Add way/generators dev dependency

--- app/config/local/app.php
+++ app/config/local/app.php
@@ -28,4 +28,19 @@ return array(
 
 	'url' => 'http://image-board.dev',
 
+	/*
+	|--------------------------------------------------------------------------
+	| Autoloaded Service Providers
+	|--------------------------------------------------------------------------
+	|
+	| The service providers listed here will be automatically loaded on the
+	| request to your application. Feel free to add your own services to
+	| this array to grant expanded functionality to your applications.
+	|
+	*/
+
+	'providers' => array(
+		'Way\Generators\GeneratorsServiceProvider'
+	),
+
 );
